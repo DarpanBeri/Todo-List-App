98fd9f462f88ef1eb13886d6a2cae109b1a43ba8
dberi <darpanberi.99@gmail.com>
2019-06-07 15:50:10 -0500

    Updates

---
+++ precache-manifest.0df0d8a1626e8f920fd8e1bb35760ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fab04e85d6e252d492e15a5eb28bb31",
+    "url": "/Todo-List-App/index.html"
+  },
+  {
+    "revision": "d65f9665897472f0327c",
+    "url": "/Todo-List-App/static/css/main.4e74d42c.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/Todo-List-App/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "d65f9665897472f0327c",
+    "url": "/Todo-List-App/static/js/main.5cc7d64f.chunk.js"
+  },
+  {
+    "revision": "390ca683b95fb2a0d7aa",
+    "url": "/Todo-List-App/static/js/runtime~main.1f6feaad.js"
+  }
+]);
